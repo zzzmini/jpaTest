2bbe6f9de50d33e2f8bb8aed1b30515282749e5b
zzzmini <sm01079200887@gmail.com>
2025-08-27 16:20:07 +0900

    Test 만들고 @Getter @Setter 담

---
+++ src/main/java/com/my/jpaTest/dto/Test.java
@@ -0,0 +1,12 @@
+package com.my.jpaTest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    public void sout() {
+        System.out.println("잘됨");
+    }
+}
